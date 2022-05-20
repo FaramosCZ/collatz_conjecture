92c39d3febf391bff8b9d107779effdf97037ba0
Michal Schorm <mschorm@redhat.com>
2022-05-20 07:18:39 +0200

    Tweak the sizes of the variables Correctly use pointer arithmetic

--- code.c
+++ code.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+// 'sleep()', 'usleep()'
 #include <unistd.h>
+// Data type 'uint8_t'
+#include <stdint.h>
 
 #define  ASCII_ZERO 48
 
-// Declare glbal pointer to memory in which each byte will represent a char.
-const unsigned short size_of_unsigned_short = sizeof(unsigned short);
-
-unsigned short *data = NULL;
-unsigned int size_of_allocated_memory;
-
-unsigned short carry = 0;
+// Declare a global pointer to memory where the digits are stored
+//   Since I work with digits 0-9, I only need 4 bits.
+//   Though it is efficent to allow a mathematical arithmetic on those digits - like multiplication - which leads to numbers larger than a single digit.
+//   Also in C language it is hard to work with less than a byte variables, so stay with 'uint8_t' to save memory, but also allow some space for the arithmetic around
+uint8_t *data = NULL;
+// Counter of how many digits the large number have
+//   We need this for easy iteration over those digits
+unsigned int number_of_digits;
+// Carry 'bit'
+//   Wherever neede in manual division or multiplication
+uint8_t carry = 0;
 
 // In each iteration two files are used
-// From one the old_value is read, to the second the new_value is written
-// The initial file is always named 'seed'.
-// This is then changed to name 'result_x', where 'x' is the 'filename_num' variable which is incremented in each iteration
-char filename[200] = "seed";
+//   From one the old_value is read, to the second the new_value is written
+//   The initial file is always named 'seed'.
+//   This is then changed to name 'result_x', where 'x' is the 'filename_num' variable which is incremented in each iteration
+char filename[30] = "seed";
 int filename_num = 1;
 
 // ------------------------------
 int load_number_from_file()
   {
+   // erase the carry 'bit' just to be sure
    carry = 0;
 
+   // Open the file
    FILE *fp;
    fp = fopen(filename, "r");
-   if ( fp == NULL )
-     {
-      fprintf(stderr, "Unable to open file");
-      return 1;
-     }
+     if ( fp == NULL ) { fprintf(stderr, "Unable to open file"); return 1; }
 
+   // Find out the number of digits in the file
+   //   This assumes the digits are 'char' of size 1 byte AND there is nothing but the digits in the file (e.g. no newline at the end of the file)
    fseek(fp, 0, SEEK_END);
-   printf("\t\t\tNumber of digits: %ld", ftell(fp));
-   size_of_allocated_memory = size_of_unsigned_short * ftell(fp) ;
-   data = malloc(size_of_allocated_memory);
+   number_of_digits = ftell(fp);
+   printf("\t\t\tNumber of digits: %d", number_of_digits);
+
+   data = malloc( number_of_digits * sizeof(uint8_t) );
    if ( data == NULL )
-     {
-      fprintf(stderr, "Unable to allocate enough memory. Tried to allocate %d bytes", size_of_allocated_memory);
-      fclose(fp);
-      return 1;
-     }
+     { fprintf(stderr, "Unable to allocate enough memory. Tried to allocate %ld bytes", number_of_digits * sizeof(uint8_t) ); fclose(fp); return 1; }
 
+   // Load the digits to the allocated memory
    fseek(fp, 0, SEEK_SET);
+   for ( unsigned int i = 0 ; i < number_of_digits ; i++ )
+     { *(data+i) = getc(fp) - ASCII_ZERO; }
 
-   for(unsigned int i = 0 ; i < (size_of_allocated_memory / size_of_unsigned_short) ; i++)
-     {
-      *(data+i) = getc(fp) - ASCII_ZERO;
-     }
-
-  if (fclose(fp))
-    {
-     printf("error closing file.");
-     exit(-1);
-    }
+  if ( fclose(fp) )
+    { printf("error closing file."); exit(-1); }
 
    return 0;
   }
@@ -62,15 +61,42 @@ int load_number_from_file()
 // ------------------------------
 int print_number(FILE * stream)
   {
-   if(carry != 0) fprintf(stream, "%c", carry+ASCII_ZERO);
+   // After multiplication we may have overflowed the the decimal order by one
+   if ( carry != 0 ) fprintf(stream, "%c", carry+ASCII_ZERO);
 
-   for(unsigned int i = 0; i < (size_of_allocated_memory / size_of_unsigned_short) ; i++)
+   for ( unsigned int i = 0; i < number_of_digits ; i++)
      {
+      // After division, we may have underflowed the decimal order
       if( i==0 && *(data+i) == 0) continue;
-      fprintf(stream, "%c", *(data+i) + ASCII_ZERO );
+
+      fprintf( stream, "%c", *(data+i) + ASCII_ZERO );
      }
 
-   if (stream == stdout ) printf("\n");
+   // Additional formatiing
+   if ( stream == stdout ) printf("\n");
+
+   return 0;
+  }
+
+// ------------------------------
+int save_number_to_file()
+  {
+   FILE *fp;
+
+   sprintf( filename, "result_%d", filename_num );
+
+   // For large numbers, it may be VERY space consuming to save every result
+   //   sometimes it might be better to disable the incrementation and stay with a single buffer file.
+//   filename_num++;
+
+   fp = fopen( filename, "w" );
+   if ( fp == NULL )
+     { fprintf(stderr, "Unable to open file '%s'", filename); return 1; }
+
+   print_number( fp );
+
+   if ( fclose(fp) )
+     { printf("error closing file."); exit(-1); }
 
    return 0;
   }
@@ -81,44 +107,19 @@ int print_number(FILE * stream)
 
 int multiply()
   {
+   // Setting the '+1' from the '3x+1' formula
    carry = 1;
-   unsigned short value;
+   uint8_t value;
 
-   for(unsigned int i = size_of_unsigned_short; i <= (size_of_allocated_memory / size_of_unsigned_short) ; i++)
+   for( unsigned int i = 1 ; i <= number_of_digits ; i++ )
      {
-       value = *(data+(size_of_allocated_memory / size_of_unsigned_short)-i);
+       value = *( data + number_of_digits - i );
        value = (value * 3) + carry;
 
        carry = value / 10;
        value = value % 10;
 
-       *(data+(size_of_allocated_memory / size_of_unsigned_short)-i) = value;
-     }
-
-   return 0;
-  }
-
-// ------------------------------
-int save_number_to_file()
-  {
-   FILE *fp;
-
-   sprintf(filename,"result_%d",filename_num);
-   //filename_num++;
-
-   fp = fopen(filename, "w");
-   if ( fp == NULL )
-     {
-      fprintf(stderr, "Unable to open file '%s'", filename);
-      return 1;
-     }
-
-   print_number(fp);
-
-   if (fclose(fp))
-     {
-      printf("error closing file.");
-      exit(-1);
+       *( data + number_of_digits - i ) = value;
      }
 
    return 0;
@@ -127,18 +128,20 @@ int save_number_to_file()
 // ------------------------------
 int divide()
   {
+   // erase the carry 'bit'
    carry = 0;
-   unsigned short value;
+   uint8_t value;
 
-   for(unsigned int i = 0; i < (size_of_allocated_memory / size_of_unsigned_short) ; i++)
+   for( unsigned int i = 0 ; i < number_of_digits ; i++ )
      {
-      value = *(data+i) + carry*10;
+      value = *( data + i ) + ( carry * 10 );
       carry = value % 2;
       value = value / 2;
 
-      *(data+i) = value;
+      *( data + i ) = value;
      }
 
+   // erase the carry 'bit' again, so no leftover is left behind for the 'print_number()' function
    carry = 0;
    return 0;
   }
@@ -146,17 +149,19 @@ int divide()
 // ------------------------------
 int main()
   {
-
+   // Iteration counter
+   //   useful when we don't want to store every result, we just want to keep the final one
    unsigned long iteration = 1;
 
    do
      {
       printf("Iteration: %lu", iteration++);
 
-       load_number_from_file();
-       //print_number(stdout);
+      load_number_from_file();
+      // For numbers with milions of digits we might not want to print every one of them
+//      print_number(stdout);
 
-       if( *(data + (size_of_allocated_memory / size_of_unsigned_short) - size_of_unsigned_short) % 2 == 0 )
+       if( *( data + number_of_digits - 1 ) % 2 == 0 )
          {
           printf("\t\t\tD\n");
           divide();
@@ -166,16 +171,15 @@ int main()
           printf("\t\t\tM\n");
           multiply();
          }
-//       print_number(stdout);
 
        save_number_to_file();
        free(data);
        data = NULL;
 
-       //usleep(1000*10);
+       // Artificial delay for debugging
+//       usleep(1000*10);
     }
-    // this means WHILE the result is more than one digit long
-    while(size_of_allocated_memory >= size_of_unsigned_short);
+    while( number_of_digits > 1 );
 
   return 0;
   }
